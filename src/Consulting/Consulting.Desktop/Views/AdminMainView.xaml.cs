0ef7911697b8145afb510a4d126798d2423a99f6
Vladimir Stroganov <vlastroganov@gmail.com>
2024-05-09 16:52:01 +0300

    Remove unused code

--- src/Consulting/Consulting.Desktop/Views/AdminMainView.xaml.cs
+++ src/Consulting/Consulting.Desktop/Views/AdminMainView.xaml.cs
@@ -1,27 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Consulting.Desktop.Views
-{
-    /// <summary>
-    /// Interaction logic for AdminMainView.xaml
-    /// </summary>
-    public partial class AdminMainView : UserControl
-    {
-        public AdminMainView()
-        {
+namespace Consulting.Desktop.Views {
+    public partial class AdminMainView : UserControl {
+        public AdminMainView() {
             InitializeComponent();
         }
     }
